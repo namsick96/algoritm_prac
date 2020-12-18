ffc5e305e9c10bf8ffe7ab665091d65eb181bd27
namsick96 <namsick96@naver.com>
2020-12-18 21:49:52 +0900

    [feat] solve Baekjoon#1316 문자열,실버5,쉽게 품

--- groupWordChecker_1316.py
+++ groupWordChecker_1316.py
@@ -5,5 +5,24 @@ with open("test.txt", "r") as f:
     list1 = []
     for k in range(n):
         list1.append(f.readline().rstrip())
+        # 리스트에 입력값 받음.
+    count=0
+    for k in range(n):
+        target=list1[k]
+        length=len(target)
+        checked_char=[]
+        groupString=True
+        for p in range(length):
+            if (target[p] in checked_char):
+                if (target[p-1]==target[p]):
+                    continue
+                else:
+                    groupString=False
+                    break
 
+            else:
+                checked_char.append(target[p])
+        if (groupString):
+            count=count+1
+    print(count)
 
