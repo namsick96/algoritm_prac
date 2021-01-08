652a9f3932d0ec4399b1f63475520e9f91e7c10d
namsick96 <namsick96@naver.com>
2021-01-08 12:22:02 +0900

    [feat] solve Baekjoon#7568 브루트 포스, 실버5, 하, 쉽게 풀었다. 그냥 다 비교하면 됨.

---
+++ bruteForce/humanSize_7568.py
@@ -0,0 +1,23 @@
+import sys
+
+f=open('../test.txt','r')
+
+num=int(f.readline().strip())
+wholelist=[]
+for k in range(num):
+    element=list(map(int,f.readline().strip().split()))
+    wholelist.append(element)
+
+ranklist=[]
+for k in range(num):
+    count=1
+    weight=wholelist[k][0]
+    height=wholelist[k][1]
+    for j in range(num):
+        otherweight=wholelist[j][0]
+        otherheight=wholelist[j][1]
+        if (otherheight>height and otherweight>weight):
+            count+=1
+    ranklist.append(count)
+for k in ranklist:
+    print(k,end=' ')
