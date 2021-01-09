24d897c4cbf69a8fc9865f40622017eeb0535f49
namsick96 <namsick96@naver.com>
2021-01-09 14:53:11 +0900

    [feat] solve Baekjoon#2751 정렬, 실버5, 중, 구현은 파이썬으로 merge sort를 했는데 시간초과가 되어서 백준에서는 안먹혔다. 백준에서는 내장 sort함수 써서 풀어야 됨. 이거는 나중에 c++로 다시 해보자.

---
+++ sorting/sortingNumber2_2751.py
@@ -0,0 +1,44 @@
+def merge(leftlist,rightlist):
+    result=[]
+    while len(leftlist)>0 or len(rightlist)>0:
+        if len(leftlist)>0 and len(rightlist)>0:
+            if leftlist[0]<=rightlist[0]:
+                result.append(leftlist[0])
+                leftlist=leftlist[1:]
+            else:
+                result.append(rightlist[0])
+                rightlist=rightlist[1:]
+        elif len(leftlist)>0 and len(rightlist)==0:
+            result=result+leftlist
+            leftlist=[]
+        elif len(leftlist)==0 and len(rightlist)>0:
+            result=result+rightlist
+            rightlist=[]
+
+    return result
+
+def merge_sort(list1,left,right):
+    mid=len(list1)//2
+    leftlist=list1[:mid]
+    rightlist=list1[mid:]
+    if len(list1)<=1:
+        return list1
+
+    if (left<right):
+        leftlist=merge_sort(leftlist,0,mid-1)
+        rightlist=merge_sort(rightlist,0,len(rightlist)-1)
+    merged_list=merge(leftlist,rightlist)
+    return merged_list
+
+
+
+f=open('../test.txt','r')
+
+num=int(f.readline().strip())
+
+wholelist=[]
+for k in range(num):
+    wholelist.append(int(f.readline().strip()))
+
+ans=merge_sort(wholelist,0,len(wholelist)-1)
+print(ans)
