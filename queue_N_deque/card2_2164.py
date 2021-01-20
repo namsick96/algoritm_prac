1974f6949991759c14166a03c101518b20cb412f
namsick96 <namsick96@naver.com>
2021-01-20 20:40:30 +0900

    [feat] solve Baekjoon# 2164 큐,덱, 실버4,중, N==1일 경우 케이스 생각을 안함. 항상 n=0,n=1,n==inf일 경우를 생각해 주기.

---
+++ queue_N_deque/card2_2164.py
@@ -0,0 +1,32 @@
+import sys
+class Deque():
+    def __init__(self,n):
+        self.items=[k for k in range(1,n+1)]
+        self.idx=0
+        self.length=n
+
+    def shuffle(self):
+        if self.length==1:
+            print(1)
+            return False
+
+        last=self.items[-1]
+        self.idx+=1
+        self.items.append(self.items[self.idx])
+        self.idx+=1
+        if last==self.items[-1]:
+            print(last)
+            return False
+        return True
+
+
+
+f=open('../test.txt','r')
+num=int(f.readline().strip())
+
+deque=Deque(num)
+
+go=True
+while(go):
+    go=deque.shuffle()
+
