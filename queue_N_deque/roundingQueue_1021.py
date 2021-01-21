7b4232c16b29b894d21c47e037b19257b7b28e79
namsick96 <namsick96@naver.com>
2021-01-21 19:29:34 +0900

    [feat] solve Baekjoon# 1021 큐,덱, 실버4, 중, 나는 멍청이다 13번째 라인에서 변수 잘못 설정해놔서 계속 헤멤. 앞으로는 이러지 말자..

---
+++ queue_N_deque/roundingQueue_1021.py
@@ -0,0 +1,31 @@
+import sys
+from collections import deque
+
+f=open('../test.txt','r')
+a,b=map(int,f.readline().split())
+items=[k for k in range(1,a+1)]
+line=list(map(int,f.readline().split()))
+deque1=deque(items)
+count=0
+for i in range(len(line)):
+    length=len(deque1)
+    #이줄에서 if items[0]이라고 해서 한참 헤멤
+    if deque1[0]==line[i]:
+        deque1.popleft()
+        continue
+
+    idx = deque1.index(line[i])
+    if idx<=length//2:
+        #rm
+        deque1.rotate(-1*idx)
+        deque1.popleft()
+        count=count+idx
+        continue
+    else:
+        #lm
+        deque1.rotate((length-idx))
+        deque1.popleft()
+        count=count+length-idx
+        continue
+
+print(count)
