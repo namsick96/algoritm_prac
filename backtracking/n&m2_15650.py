f6a102a00e46451cdf0af937a7df4a6b6cd4ff64
namsick96 <namsick96@naver.com>
2021-01-22 18:59:03 +0900

    [feat] solve Baekjoon# 15650 백트래킹, 실버3, 중, itertools안쓰고 dfs로 구현함. n==1일때 체크해줘야함. dfs연습하자!!

---
+++ backtracking/n&m2_15650.py
@@ -0,0 +1,38 @@
+import sys
+
+def dfs_comb2(lst, n):
+    #dfs인 경우 n==1이면 이렇게 해줘야함.
+    if n==1:
+        return [[k] for k in lst]
+    ret = []
+    idx = [i for i in range(len(lst))]
+
+    stack = []
+    for i in idx[:len(lst) - n + 1]:
+        stack.append([i])
+
+    while len(stack) != 0:
+        cur = stack.pop()
+
+        for i in range(cur[-1] + 1, len(lst) - n + 1 + len(cur)):
+            temp = cur + [i]
+            if len(temp) == n:
+                element = []
+                for i in temp:
+                    element.append(lst[i])
+                ret.append(element)
+            else:
+                stack.append(temp)
+    return ret
+
+f=open('../test.txt','r')
+a,b=map(int,f.readline().strip().split())
+
+
+
+ans=dfs_comb2([k for k in range(1,a+1)],b)
+ans.sort()
+for i in ans:
+    s=" ".join(map(str,i))
+    print(s)
+
